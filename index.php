ffe5ea25277c5c9cc3fd39d19018999f928a5cea
lekman12 <nurklsmajid@gmail.com>
2023-10-04 09:46:33 +0700

    menambahkan judul

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 <body>
     <h1>hello world</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, placeat aperiam ut voluptas eligendi quas debitis est dolores distinctio minima nihil non sint sed, ea illum ipsam cumque fuga odio!</p>
+    <h2>Data mahasiswa</h2>
 </body>
 </html>
